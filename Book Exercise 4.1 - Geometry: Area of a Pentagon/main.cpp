3c584dc7d3b69f7bd85c81de88a43ad691086831
alexberry721 <alexberry721@gmail.com>
2016-09-12 09:18:59 -0700

    Init commit

--- Book Exercise 4.1 - Geometry: Area of a Pentagon/main.cpp
+++ Book Exercise 4.1 - Geometry: Area of a Pentagon/main.cpp
@@ -7,9 +7,56 @@
 //
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    cout << "Geometry: Area of a Pentagon \n";
+    
+    // Data and Initialization
+    const double PI = 3.14159;
+    // side length
+    double s = 0.0;
+    
+    // modulo off the decimal numbers of s
+    double s_dec = 0;
+    
+    // length, center to vertex
+    double r = 0.0;
+    // area output of calculation
+    double area = 0.0;
+    
+    // I
+    // "Enter the length of the center of a pentago to a vertex: "
+    cout << "Enter the length of the center of a pentago to a vertex: ";
+    cin >> r;
+    
+    // P
+    /*  
+        compute the area of the pentagon
+        5 * s^2 / 4 * tan(PI/5)
+        r is the length of a side
+        side computed by s = 2r sin(PI/5) where r is the length from
+        the center of the pentagon to a vertex
+        round up the two digits after the decimal point
+    */
+    
+    // compute the side so we can use it in the formula
+    s = (2 * r) * sin(PI / 5);
+    cout << "side: " << s << endl;
+    
+    // round up the two digits after the decimal of area
+    s_dec = s % s;
+    cout << "s_dec: " << s_dec << endl;
+    
+    // compute the formula
+    area = (5 * pow(s, 2.0)) / (4 * tan(PI / 5));
+    
+    
+    // O
+    // "The area of the pentagon is: "
+    cout << "area: " << area << endl;
+    
     return 0;
 }
